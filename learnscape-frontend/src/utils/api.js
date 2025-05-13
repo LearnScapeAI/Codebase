60673ccc8e04babf219d5bfec8bc3457b1535b3d
Ashish Lathkar <63287421+Ashishlathkar77@users.noreply.github.com>
2025-05-12 20:12:51 -0400

    Delete learnscape-frontend directory

--- learnscape-frontend/src/utils/api.js
+++
@@ -1,7 +0,0 @@
-// utils/api.js
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-export const fetchRoadmaps = async () => {
-  const response = await fetch(`${API_BASE_URL}/roadmaps`);
-  return await response.json();
-};
